10714269b0a451e938e962488c2e1a8d492a5ebd
IHasBone <89635515+IHasBone@users.noreply.github.com>
2023-05-19 18:31:55 +0200

    Fixed loading file in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <sstream>
+
 #include "../include/lexer/lexer.h"
 #include "../include/parser/parser.h"
 #include "../include/classes/tokenizer_pair.h"
@@ -5,9 +8,13 @@
 
 
 int main(int argc, char** argv) {
-    std::string file = argv[1];
+    std::string f = argv[1];
+    std::ifstream file(f);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string source = buffer.str();
     
-    std::vector<pair> tokens = lex(file);
+    std::vector<pair> tokens = lex(source);
     Node* ast = parse(tokens);
     interpret(ast);
 
