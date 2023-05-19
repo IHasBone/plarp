7c5aedbc9ed6955d82e843423ca969fcb2f031c9
IHasBone <89635515+IHasBone@users.noreply.github.com>
2023-05-19 19:17:47 +0200

    Slight progress on lexer

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,8 +1,24 @@
+#include <cctype>
+#include <stdexcept>
 #include "../../include/lexer/lexer.h"
 
 
 std::vector<pair> lex(const std::string& source) {
     std::vector<pair> tokens;
+
+    int column = 0;
+    int row = 0;
+
+    for(char c : source) {
+        column++;
+        row++;
+
+        switch (c) {
+            default:
+                std::string c_string{c};
+                throw std::runtime_error("Invalid character '" + c_string + "' at column " + std::to_string(column) + ", row " + std::to_string(row));
+        }
+    }
 
     return tokens;
 }
